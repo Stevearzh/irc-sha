1b9b443fe09f884a4445d7b73d468ef4fd3e09fc
Stevearzh <zy911004@gmail.com>
2017-06-23 00:06:35 +0800

    update

--- isha/model/event/set_max_hp.py
+++ isha/model/event/set_max_hp.py
@@ -1,13 +1,12 @@
 from isha.core.event import event
 from isha.dict.event import EVENT
-from isha.dict.identity import IDENTITY
 
 class event_set_max_hp(event):
     def __init__(self):
         event.__init__(self, EVENT['set_max_hp'], self.__effect)
 
     def __effect(self, player, table, max_hp):
-        if len(table.players()) >= 5 and player.id() == IDENTITY['king']:
+        if len(table.players()) >= 5 and player.is_king():
             player.set_initial_hp(max_hp + 1)
         else:
             player.set_initial_hp(max_hp)
